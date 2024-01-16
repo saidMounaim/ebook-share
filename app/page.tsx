31d7c180b1c9de6f8c7349bfdc40c634f4052416
saidMounaim <saidmounaim00@gmail.com>
2024-01-16 10:18:05 +0100

    Fetch Books

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
+import prisma from "../lib/prisma";
+
+async function getBooks() {
+  const books = await prisma?.book.findMany({});
+  return books;
+}
+
+export default async function Home() {
+  const books = await getBooks();
+
+  console.log(books);
+
   return (
-    <>
-      <h1>E-Book Share</h1>
-    </>
+    <main className="max-w-4xl mx-auto my-5">
+      <h1 className="text-3xl font-bold ">Latest Books</h1>
+    </main>
   );
 }
